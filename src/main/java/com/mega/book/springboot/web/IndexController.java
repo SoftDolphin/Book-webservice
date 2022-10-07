a20938800067fbd37d174cf7a601978d8524870d
SoftDolphin <hawrang17@naver.com>
2022-10-07 13:35:43 +0900

    AWS 6일차

--- src/main/java/com/mega/book/springboot/web/IndexController.java
+++ src/main/java/com/mega/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.mega.book.springboot.web;
 
+import com.mega.book.springboot.config.auth.LoginUser;
 import com.mega.book.springboot.config.auth.dto.SessionUser;
 import com.mega.book.springboot.service.posts.PostsService;
 import com.mega.book.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             System.out.println(user.getName());
             model.addAttribute("userName1", user.getName());
